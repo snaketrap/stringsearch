f916498c6c2c0fa0445fc4b1f6c0daf084166fce
snaketrap <b.sarnecki11@gmail.com>
2022-06-09 10:11:40 +0200

    Dodaj pliki projektów.

---
+++ stringsearch/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace stringsearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
